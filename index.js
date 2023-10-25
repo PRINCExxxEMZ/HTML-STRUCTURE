3c62b2d8b84c9fe96aa188196d1f0d7918eee334
Adeoye Emmanuel <Freshprinceemz@gmail.com>
2023-10-25 16:28:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,121 @@
+// intro to js
+// let count = 5
+// count = count + 1 
+
+
+// console.log(count);
+
+// human to dog years
+
+// let myAge = 21
+// let humanDogRation = 7
+
+// dogYear = myAge * humanDogRation
+// console.log(dogYear)
+
+//B point
+// let bonusPoint = 50
+// console.log(bonusPoint)
+
+// bonusPoint = bonusPoint + 50
+// console.log(bonusPoint)
+
+// bonusPoint = bonusPoint - 75
+// console.log(bonusPoint)
+
+// bonusPoint = bonusPoint + 45 
+// console.log(bonusPoint)
+
+// Adding a button
+// function increment() {
+//     console.log("this is active")
+// }
+
+
+//new
+// function number(){
+//     console.log(42)
+// }
+
+// number()
+
+//laps cal
+// let lap1 =34
+// let lap2 =33
+// let lap3 =36
+
+// function lapCal() {
+//     totalLap = lap1 + lap2 + lap3
+//     console.log(totalLap)
+// }
+
+// lapCal()
+
+//lapsSum
+// let lapsCompleted = 0
+
+// function lapsSum(){
+//     lapsCompleted = lapsCompleted + 1
+
+// }
+
+// lapsSum()
+// lapsSum()
+// lapsSum()
+
+// console.log(lapsCompleted)
+
+//people counter
+
+// let countEl = document.getElementById("count-el")
+
+// console.log(countEl)
+
+// let count = 0
+
+// function increment() {
+//     count = count + 1
+//     countEl.innerText = count
+// }
+// increment()
+
+//firstLastName
+// let firstName = "Emma"
+// let LastName = "Adeoye"
+
+// let FullName = firstName + " "+ LastName
+
+// console.log(FullName)
+
+//greeting
+// let name = "linda"
+// let greeting ="Hi there"
+
+// function greetlinda() {
+//     console.log (greeting + ", " + name + " !")
+// }
+// greetlinda()
+
+//increment
+// let myPoint = 3
+
+// function add(){
+//     myPoint += 3
+// }
+// function sub(){
+//     myPoint -= 1
+// }
+// //run
+// add()
+// add()
+// add()
+// add()
+// sub()
+// sub()
+// sub()
+// sub()
+// sub()
+// console.log(myPoint)
+
+//error msg
+
